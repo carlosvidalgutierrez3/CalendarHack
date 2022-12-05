509da07f2565cf39710ba1c908c302da9bd306f1
carlosvidalgutierrez3 <carlosvidalgutierrez3@gmail.com>
2022-12-05 22:12:45 +0200

    added another h

--- edit_readme.sh
+++ edit_readme.sh
@@ -3,23 +3,34 @@
 character="h"
 filename="README.md"
 
-if [ ! -f "$filename" ]; then
-    echo "File $filename does not exist."
-    exit 1
-fi
+for i in {1..5}
+do
+    echo -n "$character" >> "$filename"
+    echo "Character '$character' added to $filename."
 
-echo -n "$character" >> "$filename"
-echo "Character '$character' added to $filename."
+    sleep 1
 
-sleep 1
+    git add *
 
-git add *
+    sleep 2
 
-sleep 2
+    git commit -m "added another h"
 
-git commit -m "added another h"
+    sleep 3
 
-sleep 3
+    random_hh=$((RANDOM % 13 + 10))
+    random_mm=$((RANDOM % 49 + 10))
+    random_ss=$((RANDOM % 49 + 10))
+    time_clock="$random_hh:$random_mm:$random_ss"
+    date_full="Mon Dec 5 $time_clock 2022"
 
-GIT_COMMITTER_DATE="Mon Dec 5 20:46:10 2022 +0200" git commit --amend --no-edit --date "Mon Dec 5 20:46:10 2022 +0200"
+    GIT_COMMITTER_DATE="$date_full +0200" git commit --amend --no-edit --date "$date_full +0200"
+
+    sleep 3
+
+    git push
+
+
+done
+
 
