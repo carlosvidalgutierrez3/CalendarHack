f65dad6df5e11103c7f8fcc3104605f7df87e1ab
carlosvidalgutierrez3 <carlosvidalgutierrez3@gmail.com>
2023-01-03 16:44:44 +0200

    added another h

--- edit_readme.sh
+++ edit_readme.sh
@@ -24,7 +24,7 @@ do
     random_mm=$((RANDOM % 49 + 10))
     random_ss=$((RANDOM % 49 + 10))
     time_clock="$random_hh:$random_mm:$random_ss"
-    date_full="Tue Jan 2 $time_clock 2023"
+    date_full="Tue Jan 3 $time_clock 2023"
 
     GIT_COMMITTER_DATE="$date_full +0200" git commit --amend --no-edit --date "$date_full +0200"
 
