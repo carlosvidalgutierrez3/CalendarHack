b5570b18db97723bc298a2213a634c4d3c25263c
carlosvidalgutierrez3 <carlosvidalgutierrez3@gmail.com>
2023-01-11 16:23:41 +0200

    added another h

--- test.sh
+++ test.sh
@@ -3,36 +3,11 @@
 character="h"
 filename="README.md"
 
-for i in {1..5}
+# Define the list of values
+values=(16 20)
+
+# Iterate over each value in the list
+for value in "${values[@]}"
 do
-    echo "$i"
-
-    echo -n "$character" >> "$filename"
-    echo "Character '$character' added to $filename."
-
-    sleep 1
-
-    git add *
-
-    sleep 2
-
-    git commit -m "added another h"
-
-    sleep 3
-
-    random_hh=$((RANDOM % 13 + 10))
-    random_mm=$((RANDOM % 49 + 10))
-    random_ss=$((RANDOM % 49 + 10))
-    time_clock="$random_hh:$random_mm:$random_ss"
-    date_full="Tue Jan 6 $time_clock 2023"
-
-    GIT_COMMITTER_DATE="$date_full +0200" git commit --amend --no-edit --date "$date_full +0200"
-
-    sleep 3
-
-    git push
-
-    sleep 5
-
-
+    echo "Value: $value"
 done
