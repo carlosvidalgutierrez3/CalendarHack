6cb0285f334984fab3e9522a5cf4ad145e428c15
carlosvidalgutierrez3 <carlosvidalgutierrez3@gmail.com>
2023-03-03 19:12:54 +0200

    added another h

--- edit_readme.sh
+++ edit_readme.sh
@@ -4,7 +4,7 @@ character="h"
 filename="README.md"
 
 # Days of the month
-days=(1 2 3 10 20 21 22 23 24)
+days=(3 13 14 15 16 17 20 24 27 28 29 30 31)
 
 for day in "${days[@]}"
 do
@@ -16,30 +16,30 @@ do
     echo -n "$character" >> "$filename"
     echo "Character '$character' added to $filename."
 
-    sleep 1
+    # sleep 1
 
     git add *
 
-    sleep 1
+    # sleep 1
 
     git commit -m "added another h"
 
-    sleep 1
+    # sleep 1
 
     random_hh=$((RANDOM % 13 + 10))
     random_mm=$((RANDOM % 49 + 10))
     random_ss=$((RANDOM % 49 + 10))
     time_clock="$random_hh:$random_mm:$random_ss"
-    date_full="Tue Feb $day $time_clock 2023"
+    date_full="Tue Mar $day $time_clock 2023"
 
     GIT_COMMITTER_DATE="$date_full +0200" git commit --amend --no-edit --date "$date_full +0200"
 
-    sleep 1
+    # sleep 1
 
 
 done
     git push
 
-    sleep 5
+    # sleep 5
 done
 
