f4ec42d9f09d271dc20a7e3a84cc28a81258e42a
Carlos Vidal <carvi03@liu.se>
2023-08-30 09:15:16 +0200

    Add description of the code

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-TODO:
+# CalendarHack
+The script *edit_readme.sh* will add git commits in the specified dates, allowing you to, for example, represent text in your github calendar.
 
+## TODO
 Change input of the commit dates, eg: read text from a text file and generate the dates to commit in the calendar.
--- edit_readme.sh
+++ edit_readme.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+# The script *edit_readme.sh* will add git commits in the specified dates,
+# allowing you to, for example, represent text in your github calendar.
 
+# Each commit will add one character (h) to the specified filename
 character="h"
 filename="README.md"
 
-# Days of the month
+# Days of the month to commit
 days=(1 2 3 4 7 11 15 16 17)
+# Month to commit
+month="Aug"
 
 for day in "${days[@]}"
 do
 
+# Number of commits. In this case 5 for each date
 for i in {1..5}
 do
     echo "$i"
@@ -16,30 +22,23 @@ do
     echo -n "$character" >> "$filename"
     echo "Character '$character' added to $filename."
 
-    # sleep 1
-
     git add *
-
-    # sleep 1
 
     git commit -m "added another h"
 
-    # sleep 1
-
+    # Generate random time of the day
     random_hh=$((RANDOM % 13 + 10))
     random_mm=$((RANDOM % 49 + 10))
     random_ss=$((RANDOM % 49 + 10))
     time_clock="$random_hh:$random_mm:$random_ss"
-    date_full="Tue Aug $day $time_clock 2023"
+    date_full="Tue $month $day $time_clock 2023"
 
+    # Change commit date
     GIT_COMMITTER_DATE="$date_full +0200" git commit --amend --no-edit --date "$date_full +0200"
-
-    # sleep 1
 
 
 done
     git push
 
-    # sleep 5
 done
 
