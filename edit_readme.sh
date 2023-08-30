156399932f945f11412637b15d651322e20b92c4
Carlos Vidal <carvi03@liu.se>
2023-08-30 09:38:38 +0200

    Add comment on weekday, and make a variable with the time zone

--- edit_readme.sh
+++ edit_readme.sh
@@ -8,8 +8,11 @@ filename="README.md"
 
 # Days of the month to commit
 days=(1 2 3 4 7 11 15 16 17)
-# Month to commit
+# Month and year to commit
 month="Aug"
+year="2023"
+# CET summer-adjusted timezone (UTC+2)
+timezone="+0200"
 
 for day in "${days[@]}"
 do
@@ -31,10 +34,10 @@ do
     random_mm=$((RANDOM % 49 + 10))
     random_ss=$((RANDOM % 49 + 10))
     time_clock="$random_hh:$random_mm:$random_ss"
-    date_full="Tue $month $day $time_clock 2023"
+    date_full="Tue $month $day $time_clock $year" # the week day "Tue" has no effect
 
     # Change commit date
-    GIT_COMMITTER_DATE="$date_full +0200" git commit --amend --no-edit --date "$date_full +0200"
+    GIT_COMMITTER_DATE="$date_full +0200" git commit --amend --no-edit --date "$date_full $timezone"
 
 
 done
